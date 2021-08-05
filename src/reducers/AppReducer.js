59c9daa46f545b0a366fffde69b4b86fc73746b6
19matu91 <19matu91@gmail.com>
2021-08-05 23:04:28 +0200

    [Comment]AppReducer

--- src/reducers/AppReducer.js
+++ src/reducers/AppReducer.js
@@ -3,6 +3,7 @@ const AppReducer = (state, action) => {
         case "UPDATE_APP":
             return { ...state, ...action.data }
         case "VALORATE":
+            // Si ya esta en el listado la pelicula, añado la nueva valoracion sino añado la pelicula
             return { ...state, mylist: state.mylist.find(x => x.id === action.data.id) ? state.mylist.map(x => x.id === action.data.id ? action.data : x) : [...state.mylist, action.data] }
         default:
             return null
